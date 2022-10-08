e54f80c46dc37426e71e5249071725e2d74a8000
Raj <rajsecret14@gmail.com>
2022-10-08 14:36:41 +0530

    fd

--- list.py
+++ list.py
@@ -1,11 +1,16 @@
 # list - odered 
+from traceback import print_tb
+
+
 a = [1,5,2,6,9]
 print(a)
+
 # (indexing starts from 0)
 # two types of indexing- 1. positive, zero and 2. negative  
 print(a[2])
 print(a[-2]) 
 print(a[0])
+
 # changing the one value of list 
 a[2]=8900
 print(a)
@@ -13,3 +18,6 @@ print(a)
 b =["aditya ",1, False,5.0,0]
 print(b)
 print(b)
+# list slicing 
+friends = ["mohan","sam","45"]
+print(friends[0:3])
