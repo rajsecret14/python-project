aa830bb8f548909d800c62ef56735ba8b29737f5
Raj <rajsecret14@gmail.com>
2022-10-08 14:17:56 +0530

    lets start

---
+++ list.py
@@ -0,0 +1,15 @@
+# list - odered 
+a = [1,5,2,6,9]
+print(a)
+# (indexing starts from 0)
+# two types of indexing- 1. positive, zero and 2. negative  
+print(a[2])
+print(a[-2]) 
+print(a[0])
+# changing the one value of list 
+a[2]=8900
+print(a)
+# we can create a list with items of different types
+b =["aditya ",1, False,5.0,0]
+print(b)
+print(b)
