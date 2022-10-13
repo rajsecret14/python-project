54cf432b6c1e6cc726b2589f8d1aef0b7bcb3d05
Raj <rajsecret14@gmail.com>
2022-10-13 16:43:17 +0530

    10oct.16:47

---
+++ chapter7_loops.py/07_continue_statement.py
@@ -0,0 +1,7 @@
+for i in range (10):
+    if i<5:
+        continue 
+    print(i)
+
+# else: #excute only when loop exit naturally
+#     print("this is inside else of for.")
---
+++ chapter7_loops.py/08_pass_staement.py
@@ -0,0 +1,8 @@
+i = 4
+if i < 5:
+   pass
+while i < 10:
+      pass
+def run(player):
+    pass
+print("harry is good boy")
