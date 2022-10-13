8a7b8394c0153c01131c4d9bd4836817c2042166
Raj <rajsecret14@gmail.com>
2022-10-13 10:15:20 +0530

    date - 13 oct

---
+++ chapter7_loops.py/01_while_loop.py
@@ -0,0 +1,7 @@
+# two types of loops
+# 1. while 2. for
+i = 1
+while i <= 50:
+    print(i)
+    i = i + 1
+
---
+++ chapter7_loops.py/06_for_with_else.py
@@ -0,0 +1,5 @@
+for i in range (10):
+    print(i)
+else:
+    print("this is inside else of for.")
+    
