50cbf1c99f3ba166c5815c5a0f976069c15946e4
Raj <rajsecret14@gmail.com>
2022-10-20 10:30:17 +0530

    20oct2020_10:30

--- chapter9_file/09_01_files.py
+++ chapter9_file/09_01_files.py
@@ -1,4 +1,4 @@
 f  = open("sample.txt","r")
-text = f.read(3)
+text = f.read(3) #read first 3 characters
 print(text)
 f.close()
---
+++ chapter9_file/another.bi
@@ -0,0 +1,3 @@
+please write this code
+and not to worry about the syntax 
+its ok to add as many lines u can make.
